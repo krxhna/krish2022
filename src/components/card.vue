e600edceacd425321ed56e5a0035da22e3498bd2
krxhna <m.krishnakanth1999@gmail.com>
2022-05-31 14:40:51 +0800

    added projects and stuff

---
+++ src/components/card.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="items-center flex flex-col justify-center">
+    <div
+      class="w-96 text-base m-3 text-left p-3 border border-gray-100 rounded-md overflow-hidden shadow-lg backdrop-blur-lg"
+    >
+      <div class="text-3xl">/ about me</div>
+      <div class="text-black">
+        I'm a software engineer and artist based in Toronto. I have great
+        interest in full-stack development, artificial intelligence,
+        human-computer interactions, and everything in between.
+      </div>
+      <div class="text-black mt-8 flex flex-col">
+        fdasfa
+
+        <div class="grid grid-cols-3">
+          <div class="flex my-2 items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M5 2a1 1 0 011 1v1h1a1 1 0 010 2H6v1a1 1 0 01-2 0V6H3a1 1 0 010-2h1V3a1 1 0 011-1zm0 10a1 1 0 011 1v1h1a1 1 0 110 2H6v1a1 1 0 11-2 0v-1H3a1 1 0 110-2h1v-1a1 1 0 011-1zM12 2a1 1 0 01.967.744L14.146 7.2 17.5 9.134a1 1 0 010 1.732l-3.354 1.935-1.18 4.455a1 1 0 01-1.933 0L9.854 12.8 6.5 10.866a1 1 0 010-1.732l3.354-1.935 1.18-4.455A1 1 0 0112 2z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            <div class="d">Javascript ES6+</div>
+          </div>
+
+          <div class="flex my-2 items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M5 2a1 1 0 011 1v1h1a1 1 0 010 2H6v1a1 1 0 01-2 0V6H3a1 1 0 010-2h1V3a1 1 0 011-1zm0 10a1 1 0 011 1v1h1a1 1 0 110 2H6v1a1 1 0 11-2 0v-1H3a1 1 0 110-2h1v-1a1 1 0 011-1zM12 2a1 1 0 01.967.744L14.146 7.2 17.5 9.134a1 1 0 010 1.732l-3.354 1.935-1.18 4.455a1 1 0 01-1.933 0L9.854 12.8 6.5 10.866a1 1 0 010-1.732l3.354-1.935 1.18-4.455A1 1 0 0112 2z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            <div class="d">Python</div>
+          </div>
+          
+          <div class="flex my-2 items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M5 2a1 1 0 011 1v1h1a1 1 0 010 2H6v1a1 1 0 01-2 0V6H3a1 1 0 010-2h1V3a1 1 0 011-1zm0 10a1 1 0 011 1v1h1a1 1 0 110 2H6v1a1 1 0 11-2 0v-1H3a1 1 0 110-2h1v-1a1 1 0 011-1zM12 2a1 1 0 01.967.744L14.146 7.2 17.5 9.134a1 1 0 010 1.732l-3.354 1.935-1.18 4.455a1 1 0 01-1.933 0L9.854 12.8 6.5 10.866a1 1 0 010-1.732l3.354-1.935 1.18-4.455A1 1 0 0112 2z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            <div class="d">Vue.js</div>
+          </div>
+          
+          <div class="flex my-2 items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M5 2a1 1 0 011 1v1h1a1 1 0 010 2H6v1a1 1 0 01-2 0V6H3a1 1 0 010-2h1V3a1 1 0 011-1zm0 10a1 1 0 011 1v1h1a1 1 0 110 2H6v1a1 1 0 11-2 0v-1H3a1 1 0 110-2h1v-1a1 1 0 011-1zM12 2a1 1 0 01.967.744L14.146 7.2 17.5 9.134a1 1 0 010 1.732l-3.354 1.935-1.18 4.455a1 1 0 01-1.933 0L9.854 12.8 6.5 10.866a1 1 0 010-1.732l3.354-1.935 1.18-4.455A1 1 0 0112 2z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            <div class="d">Node.js</div>
+          </div>
+          
+          <div class="flex my-2 items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M5 2a1 1 0 011 1v1h1a1 1 0 010 2H6v1a1 1 0 01-2 0V6H3a1 1 0 010-2h1V3a1 1 0 011-1zm0 10a1 1 0 011 1v1h1a1 1 0 110 2H6v1a1 1 0 11-2 0v-1H3a1 1 0 110-2h1v-1a1 1 0 011-1zM12 2a1 1 0 01.967.744L14.146 7.2 17.5 9.134a1 1 0 010 1.732l-3.354 1.935-1.18 4.455a1 1 0 01-1.933 0L9.854 12.8 6.5 10.866a1 1 0 010-1.732l3.354-1.935 1.18-4.455A1 1 0 0112 2z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            <div class="d">HTML&CSS</div>
+          </div>
+          
+          <div class="flex my-2 items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M5 2a1 1 0 011 1v1h1a1 1 0 010 2H6v1a1 1 0 01-2 0V6H3a1 1 0 010-2h1V3a1 1 0 011-1zm0 10a1 1 0 011 1v1h1a1 1 0 110 2H6v1a1 1 0 11-2 0v-1H3a1 1 0 110-2h1v-1a1 1 0 011-1zM12 2a1 1 0 01.967.744L14.146 7.2 17.5 9.134a1 1 0 010 1.732l-3.354 1.935-1.18 4.455a1 1 0 01-1.933 0L9.854 12.8 6.5 10.866a1 1 0 010-1.732l3.354-1.935 1.18-4.455A1 1 0 0112 2z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            <div class="d">Tailwindcss</div>
+          </div>
+          
+          
+        </div>
+      </div>
+      <span
+        class="absolute inset-x-0 bottom-0 h-2 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"
+      ></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex flex-col justify-center h-screen items-center ">
-    <img class="w-[90vw] my-2 absolute " src="https://firebasestorage.googleapis.com/v0/b/greenlines-69a5c.appspot.com/o/15.png?alt=media&token=50dd54f6-52c4-4250-9606-8566879385db" />
+    <img class="w-[90vw] my-2 fixed " src="https://firebasestorage.googleapis.com/v0/b/greenlines-69a5c.appspot.com/o/15.png?alt=media&token=50dd54f6-52c4-4250-9606-8566879385db" />
 
-    <div class="relative text-center">
+    <div class="relative text-center w-screen h-full ">
       <div class="text-4xl capitalize  m-3 w-screen font-sans font-semibold mx-4">
         <div
           class="font-bold text-6xl"
@@ -10,64 +10,29 @@
           Krishna Kanth
         </div>
 
-        <div class=" items-center flex justify-center">
+        <cardVue></cardVue>
+
+
+        <div class="d">software-creations</div>
+
+
+        <div class="p-5 border-2 backdrop-blur-lg grid grid-cols-3">
         
-        <div class="w-96 text-base m-3 text-left p-3 border border-gray-100 rounded-md overflow-hidden shadow-lg backdrop-blur-sm">
-        I'm a software engineer and artist based in Toronto. I have great interest in full-stack development, artificial intelligence, human-computer interactions, and everything in between.
-          <span
-    class="absolute inset-x-0 bottom-0 h-2  bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"
-  ></span>
-        </div>
+                <cardVue></cardVue>
+        <cardVue></cardVue>
+                <cardVue></cardVue>
+        <cardVue></cardVue>
         
         </div>
+
+
+        
+
+
+        
       </div>
 
     </div>
-
-<a
-  class="relative block  p-8 overflow-hidden border border-gray-100 rounded-lg"
-  href=""
->
-  <span
-    class="absolute inset-x-0 bottom-0 h-2  bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"
-  ></span>
-
-  <div class="justify-between sm:flex">
-    <div>
-      <h5 class="text-xl font-bold text-gray-900">
-        Building a SaaS product as a software developer
-      </h5>
-      <p class="mt-1 text-xs font-medium text-gray-600">By John Doe</p>
-    </div>
-
-    <div class="flex-shrink-0 hidden ml-3 sm:block">
-      <img
-        class="object-cover w-16 h-16 rounded-lg shadow-sm"
-        src="https://www.hyperui.dev/photos/man-5.jpeg"
-        alt=""
-      />
-    </div>
-  </div>
-
-  <div class="mt-4 sm:pr-8">
-    <p class="text-sm text-gray-500">
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. At velit illum
-      provident a, ipsa maiores deleniti consectetur nobis et eaque.
-    </p>
-  </div>
-
-  <dl class="flex mt-6">
-    <div class="flex flex-col-reverse">
-      <dt class="text-sm font-medium text-gray-600">Published</dt>
-      <dd class="text-xs text-gray-500">31st June, 2021</dd>
-    </div>
-
-    <div class="flex flex-col-reverse ml-3 sm:ml-6">
-      <dt class="text-sm font-medium text-gray-600">Reading time</dt>
-      <dd class="text-xs text-gray-500">3 minute</dd>
-    </div>
-  </dl>
-</a>
 
 
 
@@ -77,9 +42,13 @@
 
 <script>
 // import bottomnavbarVue from "../components/bottomnavbar.vue";
+import cardVue from '../components/card.vue';
+
+
 
 export default {
   components: {
+    cardVue
     // bottomnavbarVue,
   },
 
